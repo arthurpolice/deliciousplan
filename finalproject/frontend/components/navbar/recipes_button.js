d1ced5919f251e62c9a68195cc35e237fd08cbb2
Arthur Police <arthurpolice1@gmail.com>
2022-12-08 10:12:18 -0400

    UI adjustments for navbar.

--- finalproject/frontend/components/navbar/recipes_button.js
+++ finalproject/frontend/components/navbar/recipes_button.js
@@ -6,8 +6,10 @@ import styles from './navbar.module.css'
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useRouter } from 'next/router';
 
 export default function RecipesButton() {
+  const route = useRouter()
   const [anchorEl, setAnchorEl] = useState(null)
   const handlePopOpen = event => {
     setAnchorEl(event.currentTarget)
@@ -40,13 +42,13 @@ export default function RecipesButton() {
         onClose={handlePopClose}
         disableRestoreFocus
       >
-        <MenuItem>
+        <MenuItem onClick={() => route.push('/catalog')}>
           <Link href={'/catalog'} className={styles.menuItem}>Recipe Catalog</Link>
         </MenuItem>
-        <MenuItem>
+        <MenuItem onClick={() => route.push('/extract')}>
           <Link href={'/extract'} className={styles.menuItem}>Recipe Extractor</Link>
         </MenuItem>
-        <MenuItem>
+        <MenuItem onClick={() => route.push('/makerecipe')}>
           <Link href={'/makerecipe'} className={styles.menuItem}>Make a Recipe</Link>
         </MenuItem>
       </Menu>
