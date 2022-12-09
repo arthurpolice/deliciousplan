272b7d3b4462f27d17b8a76aa0c357759e8fda9d
Arthur Police <arthurpolice1@gmail.com>
2022-12-09 17:52:23 -0400

    New navbar element: Avatar button.

---
+++ finalproject/frontend/components/navbar/user_avatar.js
@@ -0,0 +1,40 @@
+import { Avatar } from '@mui/material'
+
+
+function stringToColor(string) {
+  let hash = 0
+  let i
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash)
+  }
+
+  let color = '#'
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff
+    color += `00${value.toString(16)}`.slice(-2)
+  }
+  /* eslint-enable no-bitwise */
+
+  return color
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0].toUpperCase()}`,
+  }
+}
+
+export default function UserAvatar({ username, handlePopOpen }) {
+  if (username) {
+    return <Avatar {...stringAvatar(`${username}`)} onClick={handlePopOpen}/>
+  }
+  else {
+    return <Avatar src='./images/user.png' onClick={handlePopOpen}/>
+  }
+}
--- finalproject/frontend/components/navbar/user_button.js
+++ finalproject/frontend/components/navbar/user_button.js
@@ -1,40 +1,10 @@
-import { Avatar } from '@mui/material'
 import { parseCookies } from 'nookies'
 import { useEffect, useState } from 'react'
 import { Menu, MenuItem } from '@mui/material'
-import Link from 'next/link'
 import LogButton from './log_button'
+import UserAvatar from './user_avatar'
 import RegisterButton from './register_button'
 import styles from './navbar.module.css'
-
-function stringToColor(string) {
-  let hash = 0
-  let i
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash)
-  }
-
-  let color = '#'
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff
-    color += `00${value.toString(16)}`.slice(-2)
-  }
-  /* eslint-enable no-bitwise */
-
-  return color
-}
-
-function stringAvatar(name) {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(' ')[0][0]}`,
-  }
-}
 
 export default function UserButton({ token, label, handleOpen, register }) {
   const [username, setUsername] = useState('-')
@@ -55,7 +25,7 @@ export default function UserButton({ token, label, handleOpen, register }) {
   console.log(token)
   return (
     <>
-      <Avatar {...stringAvatar(`${username}`)} onClick={handlePopOpen} />
+      <UserAvatar username={username} handlePopOpen={handlePopOpen} />
       <Menu
         open={open}
         anchorEl={anchorEl}
