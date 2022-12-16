bd4ae7d32d822dca4456744dffaa38094a50bca7
Arthur Police <arthurpolice1@gmail.com>
2022-12-15 20:54:26 -0400

    UI adjustment to log in button.

--- finalproject/frontend/components/navbar/register_button.js
+++ finalproject/frontend/components/navbar/register_button.js
@@ -1,11 +1,13 @@
 import { MenuItem } from '@mui/material'
 import styles from './navbar.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function RegisterButton({ register }) {
+  route = useRouter()
   if (register) {
     return (
-      <MenuItem>
+      <MenuItem onClick={() => route.push('/register')}>
         <Link className={styles.logButton} href={'/register'}>
           {register}
         </Link>
